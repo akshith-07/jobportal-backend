53091eec208da819e3cc407a81affe27e74d91c5
akshith-07 <akshith2002jobirin@gmail.com>
2023-06-26 12:08:04 +0530

    Full Implementaion  Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = 3500;
+const jobsRouter = require("./routes/jobsRouter");
+const validateJobsRouter = require("./routes/validateJobsRouter");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.DB_URL);
+const db = mongoose.connection;
+db.on("error", (errorMessage) => console.log(errorMessage));
+db.once("open", () =>
+  console.log("The Jobwebsite database has been connected successfully!!!! :)")
+);
+
+app.use("/api/v1/jobs", jobsRouter);
+app.use("/api/v1/jobs/validate", validateJobsRouter);
+
+app.listen(
+  PORT,
+  console.log(`Server listening at http://localhost:${PORT}/api/v1/jobs`)
+);
---
+++ data/initialData.js
@@ -0,0 +1,12 @@
+const initialData = [
+  {
+    jobTitle: "Software Engineer",
+    jobID: "2",
+    jobCompany: "Google",
+    jobLocation: "Chennai",
+    jobPreference: "Experienced",
+    jobSkills: "Java or C++, Python, SQL",
+  },
+];
+
+module.exports = initialData;
